eb0b4f870e86d663f6dbe5cd55d4af4c59b25d99
irtfrm <frm.tomoya@gmail.com>
2021-10-14 04:06:54 +0900

    add synthesizer and pure tone

--- app.js
+++ app.js
@@ -1,32 +1,55 @@
-const wait = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
-const playAt = async (degree, ms) => {
-    const osc = audioCtx.createOscillator();
-    osc.type = 'sine';
-    osc.frequency.value = 261.63 * Math.pow(octave, (1 / div) * degree );
-    osc.connect(audioCtx.destination);
-    const osc2 = audioCtx.createOscillator();
-    osc2.type = 'sine';
-    osc2.frequency.value = 261.63 * 2 * Math.pow(octave, (1 / div) * degree );
-    osc2.connect(audioCtx.destination);
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-    osc.start();
-    osc2.start();
+const wait = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
+const getFreq = (degree) => {
+    return 	196 * Math.pow(octave, (1 / div) * degree);
+};
+const pure = (degree) => {
+    ratio = [1, 16/15, 10/9, 6/5, 5/4, 4/3, 45/32, 3/2, 8/5, 5/3, 9/5, 15/8];
+    return 	196 * ratio[degree % 12] * (2 ** (Math.floor(degree / 12)));
+};
+const playAt = async (degree, ms) => {
+    const oscs = [];
+    const fundamental = pure(degree);
+    const weights = [0.8, 0.9, 0.9, 0.6, 0.3, 0.6, 0.2, 0.2, 0.17, 0.3, 0.05, 0.05, 0.05, 0.15, 0.02, 0.08, 0.02, 0.06, 0.01, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005, 0.005];
+    const master = 0.04;
+
+    i = 1;
+    for (const weight of weights) {
+        const osc = audioCtx.createOscillator();
+        const gainNode = audioCtx.createGain();
+        gainNode.gain.value = weight * master;
+        osc.type = 'sine';
+        osc.frequency.value = fundamental * i;
+        osc.connect(gainNode).connect(audioCtx.destination);
+        oscs.push(osc);
+        i++;
+    }
+
+    for (const osc of oscs) {
+        osc.start();
+    }
     await wait(ms);
-    osc.stop();
-    osc2.stop();
+    for (const osc of oscs) {
+        osc.stop();
+    }
 };
 const div = 12;
 const octave = 2;
-const tempo = 122;
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-const score = [
+const tempo = 210;
+
+const vigilate = [
     {
-        "degree": 0,
+        "degree": 2,
+        "duration": 0.75,
+    },
+    {
+        "degree": 4,
         "duration": 0.25,
     },
     {
-        "degree": 2,
-        "duration": 0.25,
+        "degree": 5,
+        "duration": 0.75,
     },
     {
         "degree": 4,
@@ -38,35 +61,50 @@ const score = [
     },
     {
         "degree": 7,
+        "duration": 0.25,
+    },
+    {
+        "degree": 9,
         "duration": 1,
     },
+
+    {
+        "degree": 4,
+        "duration": 0.75,
+    },
+    {
+        "degree": 6,
+        "duration": 0.25,
+    },
+    {
+        "degree": 7,
+        "duration": 0.75,
+    },
+    {
+        "degree": 6,
+        "duration": 0.25,
+    },
     {
         "degree": 7,
         "duration": 0.25,
     },
     {
-        "degree": 5,
+        "degree": 9,
         "duration": 0.25,
     },
     {
-        "degree": 4,
-        "duration": 0.25,
+        "degree": 11,
+        "duration": 1,
     },
-    {
-        "degree": 2,
-        "duration": 0.25,
-    },
-    {
-        "degree": 0,
-        "duration": 0.5,
-    },
+
 ];
 
 document.querySelector("#play").addEventListener("click", async () => {
     if (audioCtx.state === "suspended") {
         audioCtx.resume();
     }
-    for (const note of score) {
+    
+    for (const note of vigilate) {
         await playAt(note.degree, note.duration * 60000 / 0.25 / tempo);
     }
 });
